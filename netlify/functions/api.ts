becb39b871c69c21dce85759bac2c5dc92d7741c
Deborah <startumproject@gmail.com>
2026-08-19 11:55:13 +0100

    add Netlify function wrapper and config v2

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,10 @@
 import serverless from "serverless-http";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { openDb } from "../../CleverVA/db.ts";
 import { createServer } from "../../CleverVA/server.ts";
 import { createSlackUserCache, fetchSlackUsers } from "../../CleverVA/slack-users.ts";
 
-const here = path.dirname(fileURLToPath(import.meta.url));
-
-const db = openDb(path.join(here, "reminders.db"));
+const db = openDb(path.join("/tmp", "reminders.db"));
 const slackUsers = createSlackUserCache(fetchSlackUsers);
 const app = createServer(db, slackUsers);
 
